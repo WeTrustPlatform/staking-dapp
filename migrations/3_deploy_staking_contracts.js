f73ffed4d7119beabc65da34b2da0866c273f2d7
Hoang Nguyen <hoang@wetrust.io>
2019-01-30 14:41:35 -0800

    use TRST mainnet in truffle migrations

--- migrations/3_deploy_staking_contracts.js
+++ migrations/3_deploy_staking_contracts.js
@@ -2,8 +2,9 @@ const SafeMath = artifacts.require('lib/SafeMath');
 const TimeLockedStaking = artifacts.require('TimeLockedStaking');
 const TRST = artifacts.require('lib/TRST');
 
-module.exports = function(deployer) {
+module.exports = function (deployer, network) {
   deployer.deploy(SafeMath);
   deployer.link(SafeMath, TimeLockedStaking);
-  deployer.deploy(TimeLockedStaking, TRST.address);
-}
+  const trstAddress = network !== 'mainnet' ? TRST.address : '0xCb94be6f13A1182E4A4B6140cb7bf2025d28e41B';
+  deployer.deploy(TimeLockedStaking, trstAddress);
+};
