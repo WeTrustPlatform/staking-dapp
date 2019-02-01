7c3a61ad213a988bec824e47fd084f5f478e4e23
Hoang Nguyen <hoang@wetrust.io>
2019-01-31 16:12:39 -0800

    expose env to react app

--- client/src/configs.js
+++ client/src/configs.js
@@ -1,9 +1,7 @@
 const TEST_RPC_PORT = process.env.TEST_RPC_PORT || '7545';
 
 export default {
-  CMS_URL: process.env.CMS_URL || 'https://tcr.wetrust.info/api/v0',
-  WEB3_FALLBACK_PROVIDER: process.env.WEB3_FALLBACK_PROVIDER || `ws://localhost:${TEST_RPC_PORT}`,
-  NETWORK_ID: process.env.NETWORK_ID || '5777', // 5777 is ganache's default
-  TRST_ADDRESS: process.env.TRST_ADDRESS,
-  TIME_LOCKED_STAKING_ADDRESS: process.env.TIME_LOCKED_STAKING_ADDRESS,
+  CMS_URL: process.env.REACT_APP_CMS_URL || 'https://tcr.wetrust.info/api/v0',
+  WEB3_FALLBACK_PROVIDER: process.env.REACT_APP_WEB3_FALLBACK_PROVIDER || `ws://localhost:${TEST_RPC_PORT}`,
+  NETWORK_ID: process.env.REACT_APP_NETWORK_ID || '5777', // 5777 is ganache's default
 };
