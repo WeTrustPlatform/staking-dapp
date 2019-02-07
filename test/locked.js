6657664b7a6cafda699f2aa03628fb0c737694af
Hoang Nguyen <hoang@wetrust.io>
2019-02-06 17:59:31 -0800

    add tests for emergency

---
+++ test/locked.js
@@ -0,0 +1,73 @@
+const {
+  stakeAndVerifyBalances,
+  unstakeAndVerifyBalances,
+  buildBytesInput,
+} = require('./utils');
+
+const TimeLockedStaking = artifacts.require('TimeLockedStaking');
+const TRSTArtifact = artifacts.require('lib/TRST');
+
+let StakingContract;
+let TRST;
+
+beforeEach(async () => {
+  TRST = await TRSTArtifact.deployed();
+  // Deploy new contracts so that emergency flag does not affect other tests
+  StakingContract = await TimeLockedStaking.new(TRST.address);
+});
+
+const sleep = seconds => new Promise(resolve => setTimeout(resolve, seconds * 1000));
+
+const amount = '1';
+const now = Math.floor(Date.now() / 1000);
+
+contract('Unstake with time locked', (accounts) => {
+  const [staker] = accounts;
+
+  it('should fail to unstake. Then succeed in emergency', async () => {
+    const data = buildBytesInput(now + now); // very far in the future
+
+    await stakeAndVerifyBalances(staker, amount, data, TRST, StakingContract);
+    try {
+      await StakingContract.unstake(amount, data, { from: staker });
+      assert.fail('Not supposed to reach here');
+    } catch (e) {
+      assert(e.toString().includes('This stake is still locked.'));
+      await StakingContract.setEmergency(true);
+      await unstakeAndVerifyBalances(staker, amount, data, TRST, StakingContract);
+      return;
+    }
+    assert.fail('Not supposed to reach here');
+  });
+
+  it('should fail to unstake. Then succeed after passing the unlockedAt', async () => {
+    const lockedDuration = 3; // seconds
+    const data = buildBytesInput(now + lockedDuration);
+
+    await stakeAndVerifyBalances(staker, amount, data, TRST, StakingContract);
+    try {
+      await StakingContract.unstake(amount, data, { from: staker });
+      assert.fail('Not supposed to reach here');
+    } catch (e) {
+      assert(e.toString().includes('This stake is still locked.'));
+      await sleep(lockedDuration);
+      await unstakeAndVerifyBalances(staker, amount, data, TRST, StakingContract);
+    }
+  });
+});
+
+contract('Stake with emergency', (accounts) => {
+  const [staker] = accounts;
+
+  it('should succeed to stake. Then fail in emergency', async () => {
+    const data = '0x';
+    await stakeAndVerifyBalances(staker, amount, data, TRST, StakingContract);
+    await StakingContract.setEmergency(true);
+    try {
+      await StakingContract.stake(amount, data, { from: staker });
+      assert.fail('Not supposed to reach here');
+    } catch (e) {
+      assert(e.toString().includes('Cannot stake due to emergency.'));
+    }
+  });
+});
