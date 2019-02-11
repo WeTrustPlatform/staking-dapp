79447c202b44c2e7f556fdcb367d039f24d577da
Hoang Nguyen <hoang@wetrust.io>
2019-02-10 22:38:51 -0800

    update latest contracts on rinkeby

--- migrations/3_deploy_staking_contracts.js
+++ migrations/3_deploy_staking_contracts.js
@@ -6,7 +6,7 @@ module.exports = function (deployer, network, accounts) {
   deployer.deploy(SafeMath);
   deployer.link(SafeMath, TimeLockedStaking);
 
-  let trstAddress = TRST.address;
+  let trstAddress;
   // accounts[0] is both deployer and owner on testnet
   let owner = accounts[0];
   if (network === 'mainnet') {
@@ -15,6 +15,8 @@ module.exports = function (deployer, network, accounts) {
     owner = '0x0Ed8C3d25849fe3fe918A8863A04E71036cbc501';
   } else if (network === 'rinkeby') {
     trstAddress = '0x21036C54e16521B8809553956123E44054120226';
+  } else {
+    trstAddress = TRST.address;
   }
 
   deployer.deploy(TimeLockedStaking, trstAddress, owner);
