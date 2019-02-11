eb21648540080230887bcdd4512cdbd25149c1b4
Hoang Nguyen <hoang@wetrust.io>
2019-02-11 03:01:56 -0800

    remove Home and FAQ in the Navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
 import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
 import Web3Account from './Web3Account';
 import logo from '../images/wetrust-logo.svg';
 
@@ -17,19 +15,6 @@ const styles = {
 };
 
 class Navbar extends React.Component {
-  renderLink(to, text) {
-    return (
-      <Link
-        scroll={el => el.scrollIntoView({ behavior: 'smooth', block: 'end' })}
-        to={to}
-      >
-        <Button>
-          {text}
-        </Button>
-      </Link>
-    );
-  }
-
   render() {
     const { classes } = this.props;
     return (
@@ -40,8 +25,6 @@ class Navbar extends React.Component {
               <img src={logo} alt="WeTrust" className={classes.brandImage} />
             </div>
             <Web3Account />
-            {this.renderLink('#main-section', 'Home')}
-            {this.renderLink('#faq-section', 'FAQ')}
           </Toolbar>
         </AppBar>
 
