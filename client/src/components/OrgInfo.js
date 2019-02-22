4cb9b7e3306852a3a38985bb990b1530d45516ce
Hoang Nguyen <hoang@wetrust.io>
2019-02-22 13:06:04 -0800

    fix OrgInfo for Spring causes

--- client/src/components/OrgInfo.js
+++ client/src/components/OrgInfo.js
@@ -4,32 +4,57 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 
 class OrgInfo extends React.Component {
-  renderDetails(data) {
+  render501c3Info(data) {
     return (
-      <div>
-        <p>
-          {`EIN: ${data.stakingId}`}
-        </p>
-        <p>
-          {`Location: ${data.city}, ${data.state}, ${data.country}`}
-        </p>
-      </div>
+      <span>
+        {`EIN: ${data.stakingId}`}
+        <br />
+        {`Location: ${data.city}, ${data.state}, ${data.country}`}
+      </span>
     );
+  }
+
+  render501c3(data) {
+    return (
+      <p>
+        {this.render501c3Info(data)}
+      </p>
+    );
+  }
+
+  renderSpringCause(data) {
+    return (
+      <p>
+        {`${data.name} is on `}
+        <a
+          href="https://spring.wetrust.io"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+            spring.wetrust.io
+        </a>
+        <br />
+        {data.is501c3 && this.render501c3Info(data)}
+        {!data.is501c3 && `Staking ID: ${data.stakingId}`}
+      </p>
+    );
+  }
+
+  renderDetails(data) {
+    return data.isOnSpring ? this.renderSpringCause(data) : this.render501c3(data);
   }
 
   render() {
     const { data } = this.props;
     return (
-      <div>
-        <List>
-          <ListItem>
-            <ListItemText primary={
+      <List>
+        <ListItem>
+          <ListItemText primary={
               this.renderDetails(data)
             }
-            />
-          </ListItem>
-        </List>
-      </div>
+          />
+        </ListItem>
+      </List>
     );
   }
 }
