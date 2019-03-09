e115a8b0116ab3e05ae712579c94da9120670404
Hoang Nguyen <hoang@wetrust.io>
2019-03-08 16:19:41 -0800

    split Dockerfile into different networks

--- client/docker.sh
+++ client/docker.sh
@@ -15,14 +15,11 @@ DOCKER_IMAGE="$DOCKER_REPO:$1-latest"
 if [ "$1" == "testnet" ]; then
   docker build \
     -t $DOCKER_IMAGE \
-    .
+    Dockerfile.testnet
 elif [ "$1" == "mainnet" ]; then
   docker build \
     -t $DOCKER_IMAGE \
-    --build-arg REACT_APP_CMS_URL=https://staking.wetrust.io/api/v0 \
-    --build-arg REACT_APP_NETWORK_ID=1 \
-    --build-arg REACT_APP_WEB3_FALLBACK_PROVIDER=https://mainnet.infura.io/metamask \
-    .
+    Dockerfile.mainnet
 else
   echo "Please specify either testnet or mainnet!"
   exit 1
