5aed0f5cc51f0078edcd99d6f55224442aff36a2
Hoang Nguyen <hoang@wetrust.io>
2019-03-21 13:38:52 -0700

    fix default test port to avoid conflict with testrpc dev

--- client/setUpTest.js
+++ client/setUpTest.js
@@ -1,37 +1,38 @@
-const createServer = () => new Promise((resolve) => {
-  const ganche = require('ganache-cli');
-  const server = ganche.server({
-    network_id: '5777', // default ganache network
-    accounts: [
-      {
-        secretKey:
+const createServer = () =>
+  new Promise((resolve) => {
+    const ganche = require('ganache-cli');
+    const server = ganche.server({
+      network_id: '5777', // default ganache network
+      accounts: [
+        {
+          secretKey:
             '0x275ef35f55525049678090f9e32d16cbcca06f07dc4f51e297ed39a47d901981',
-        balance: '0x4563918244f40000',
-      },
-      {
-        secretKey:
+          balance: '0x4563918244f40000',
+        },
+        {
+          secretKey:
             '0x08216419c2c2e9bfaf5f88885cbe89eabb1671cae4a09311c7ddcec77e1bfb77',
-        balance: '0x4563918244f40000',
-      },
-    ],
+          balance: '0x4563918244f40000',
+        },
+      ],
+    });
+
+    const port = process.env.TEST_RPC_PORT || 7546;
+    server.listen(port, (err) => {
+      if (err) throw err;
+
+      const ws = `ws://localhost:${port}`;
+
+      console.log(`Testrpc is running ${ws}`);
+
+      // expose SERVER for teardown
+      global.__SERVER__ = server;
+
+      resolve(ws);
+    });
+
+    server.on('close', () => console.log('Testrpc is closing...'));
   });
-
-  const port = process.env.TEST_RPC_PORT || 7545;
-  server.listen(port, (err) => {
-    if (err) throw err;
-
-    const ws = `ws://localhost:${port}`;
-
-    console.log(`Testrpc is running ${ws}`);
-
-    // expose SERVER for teardown
-    global.__SERVER__ = server;
-
-    resolve(ws);
-  });
-
-  server.on('close', () => console.log('Testrpc is closing...'));
-});
 
 let ws;
 module.exports = async () => {
