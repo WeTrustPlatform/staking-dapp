516854dad5097b7a23a8d4a7b88607180c39ae9e
Hoang Nguyen <hoang@wetrust.io>
2019-04-04 18:15:15 -0700

    add universalBar links

--- packages/client/src/components/Navbar.js
+++ packages/client/src/components/Navbar.js
@@ -3,15 +3,36 @@ import { withStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Web3Account from './Web3Account';
+import HrefLink from './HrefLink';
 import logo from '../images/wetrust-logo.svg';
+import wetrustLogo from '../images/we-trust-global-logo.svg';
+import springLogo from '../images/spring-global-logo.svg';
+import cuLogo from '../images/crypto-unlocked-global-logo.svg';
+import tlcLogo from '../images/trusted-lending-circles-global-logo.svg';
 
-const styles = {
-  grow: {
-    flexGrow: 1,
-  },
-  brandImage: {
-    height: '1.5rem',
-  },
+const styles = (theme) => {
+  const navHeight = theme.mixins.toolbar.minHeight;
+  return {
+    grow: {
+      flexGrow: 1,
+    },
+    brandImage: {
+      height: '1.5rem',
+    },
+    universalBar: {
+      minHeight: navHeight * 0.8,
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      background: '#222222',
+      [theme.breakpoints.down('sm')]: {
+        display: 'none',
+      },
+    },
+    universalBarItem: {
+      margin: 'auto 36px',
+    },
+  };
 };
 
 class Navbar extends React.Component {
@@ -19,7 +40,37 @@ class Navbar extends React.Component {
     const { classes } = this.props;
     return (
       <div>
-        <AppBar>
+        <AppBar position="relative">
+          <div className={classes.universalBar}>
+            <HrefLink href="https://wetrust.io">
+              <img
+                className={classes.universalBarItem}
+                alt="WeTrust"
+                src={wetrustLogo}
+              />
+            </HrefLink>
+            <HrefLink href="https://spring.wetrust.io">
+              <img
+                className={classes.universalBarItem}
+                alt="Spring"
+                src={springLogo}
+              />
+            </HrefLink>
+            <HrefLink href="https://tlc.wetrust.io">
+              <img
+                className={classes.universalBarItem}
+                alt="TLC"
+                src={tlcLogo}
+              />
+            </HrefLink>
+            <HrefLink href="https://cryptounlocked.wetrust.io">
+              <img
+                className={classes.universalBarItem}
+                alt="CryptoUnlocked"
+                src={cuLogo}
+              />
+            </HrefLink>
+          </div>
           <Toolbar>
             <div className={classes.grow}>
               <img src={logo} alt="WeTrust" className={classes.brandImage} />
